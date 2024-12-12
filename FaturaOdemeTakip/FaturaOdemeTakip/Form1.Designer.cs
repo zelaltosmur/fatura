bebd0af1e38c477cfb521bff965997c5c1e87b10
zelaltosmur <153620414+zelaltosmur@users.noreply.github.com>
2024-12-12 20:32:55 +0300

    Add files via upload

---
+++ FaturaOdemeTakip/FaturaOdemeTakip/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace FaturaOdemeTakip
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.DateTimePicker dateTimePickerFatura;
+        private System.Windows.Forms.Button btnKontrolEt;
+        private System.Windows.Forms.Label lblSonuc;
+        private System.Windows.Forms.Timer timer;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dateTimePickerFatura = new System.Windows.Forms.DateTimePicker();
+            this.btnKontrolEt = new System.Windows.Forms.Button();
+            this.lblSonuc = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // dateTimePickerFatura
+            // 
+            this.dateTimePickerFatura.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePickerFatura.Location = new System.Drawing.Point(187, 12);
+            this.dateTimePickerFatura.Name = "dateTimePickerFatura";
+            this.dateTimePickerFatura.Size = new System.Drawing.Size(200, 22);
+            this.dateTimePickerFatura.TabIndex = 0;
+            // 
+            // btnKontrolEt
+            // 
+            this.btnKontrolEt.Location = new System.Drawing.Point(187, 52);
+            this.btnKontrolEt.Name = "btnKontrolEt";
+            this.btnKontrolEt.Size = new System.Drawing.Size(200, 30);
+            this.btnKontrolEt.TabIndex = 1;
+            this.btnKontrolEt.Text = "Ödeme Durumunu Kontrol Et";
+            this.btnKontrolEt.UseVisualStyleBackColor = true;
+            this.btnKontrolEt.Click += new System.EventHandler(this.btnKontrolEt_Click);
+            // 
+            // lblSonuc
+            // 
+            this.lblSonuc.AutoSize = true;
+            this.lblSonuc.Location = new System.Drawing.Point(12, 100);
+            this.lblSonuc.Name = "lblSonuc";
+            this.lblSonuc.Size = new System.Drawing.Size(0, 16);
+            this.lblSonuc.TabIndex = 2;
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(163, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Fatura Son Ödeme Tarihi: ";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(488, 200);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblSonuc);
+            this.Controls.Add(this.btnKontrolEt);
+            this.Controls.Add(this.dateTimePickerFatura);
+            this.Name = "Form1";
+            this.Text = "Fatura ve Ödeme Takip";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ FaturaOdemeTakip/FaturaOdemeTakip/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace FaturaOdemeTakip
+{
+    public partial class Form1 : Form
+    {
+        private DateTime faturaTarihi;
+        private bool isFaturaGeçti;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnKontrolEt_Click(object sender, EventArgs e)
+        {
+            // Kullanıcı tarafından seçilen fatura tarihi
+            faturaTarihi = dateTimePickerFatura.Value;
+
+            // Şu anki tarih
+            DateTime bugun = DateTime.Now;
+
+            timer.Start();
+
+            // kontorl amaçlı
+            if (faturaTarihi < bugun)
+            {
+                isFaturaGeçti = true;
+                lblSonuc.Text = "Fatura ödeme tarihi geçti. Geçen süre hesaplanıyor...";
+                lblSonuc.ForeColor = System.Drawing.Color.Red;
+            }
+            else
+            {
+                isFaturaGeçti = false;
+                lblSonuc.Text = "Fatura ödeme tarihi henüz gelmedi. Kalan süre hesaplanıyor...";
+                lblSonuc.ForeColor = System.Drawing.Color.Green;
+            }
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            DateTime bugun = DateTime.Now;
+            TimeSpan fark;
+
+            // Eğer fatura ödeme tarihi geçmişse, geçen süreyi hesapla
+            if (isFaturaGeçti)
+            {
+                fark = bugun - faturaTarihi;
+                lblSonuc.Text = $"Fatura ödeme tarihi geçti. {fark.Days} Gün, {fark.Hours} Saat, {fark.Minutes} Dakika, {fark.Seconds} Saniye geçti.";
+            }
+            // Eğer fatura ödeme tarihi gelmemişse, kalan süreyi hesapla
+            else
+            {
+                fark = faturaTarihi - bugun;
+                lblSonuc.Text = $"Fatura ödeme tarihi henüz gelmedi. Kalan süre: {fark.Days} Gün, {fark.Hours} Saat, {fark.Minutes} Dakika.";
+            }
+        }
+    }
+}
